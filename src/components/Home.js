9ce0aa69a94b133e99581e90baebbc5f46167605
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-07-18 04:06:04 +0530

    Home Page Created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,44 @@
 import homeAshish from '../imgs/homeAshish.jpg'
 import './styles/Home.css'
+import CardHome from "./homepageComponents/CardHome";
+
+import homeProducts from '../imgs/homeProducts.jpg';
+import furniture from '../imgs/furniture.jpg';
+import dailyEssentials from '../imgs/dailyEssentials.jpg'
+import clothingEssentials from '../imgs/clothingEssentials.jpg'
+
 const Home = () => {
     return(
-        <div>
+        <div className={'home'}>
             <div className={'jumbotron'}>
                 <img src={homeAshish} className={'homeImage'}/>
+            </div>
+            <div className={'cardContainer'}>
+                <div className={'cardContainerOne'}>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                </div>
+                <div className={'cardContainerTwo'}>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                    <CardHome title={'Top rated, premium quality | Amazon Brands & more'}
+                              image1={homeProducts} image2={furniture} image3={dailyEssentials}
+                              image4={clothingEssentials} link={'See more...'}/>
+                </div>
             </div>
         </div>
     )
--- src/components/homepageComponents/CardHome.js
+++ src/components/homepageComponents/CardHome.js
@@ -1,10 +1,17 @@
-const CardHome = (props) =>{
+import './styles/CardHome.css'
 
-    return(
-        <div>
-            <div className={}>{props.title}</div>
-            <div className={}>{props.image}</div>
-            <div className={}>{props.link}</div>
+const CardHome = (props) => {
+
+    return (
+        <div className={'card'}>
+            <div className={'title'}><h3>{props.title}</h3></div>
+            <div className={'imageContainer'}>
+                <img className={'image'} src={props.image1}/>
+                <img className={'image'} src={props.image2}/>
+                <img className={'image'} src={props.image3}/>
+                <img className={'image'} src={props.image4}/>
+            </div>
+            <div className={''}>{props.link}</div>
         </div>
     )
 
