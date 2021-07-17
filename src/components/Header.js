7ab090e391939de31f11151a5900ab6454cfb8c7
LEON6156SCOTT <tanmay777leon@gmail.com>
2021-07-17 09:49:57 +0530

    navbar styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,58 @@
-import logo from '../imgs/B1.png'
+import logo from '../imgs/amazon_PNG11.png'
+
+import SearchIcon from '@material-ui/icons/Search';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 import './styles/Header.css'
 
 const Header = () => {
 
     return (
         <div className={'navbar'}>
+
             <div className={'navbarOne'}>
-                <img src={logo} className={'logo'}/>
+                <div className={'logoContainer'}>
+                    <img src={logo} className={'logo'}/>
+                </div>
+
+                {/*<div className={'locationBar'}>*/}
+                {/*    <LocationOnIcon className={'locationIcon'}/>*/}
+                {/*    <div className={'location'}>Hello,<span>Select Your Address</span></div>*/}
+                {/*</div>*/}
+
                 <div className={'searchBar'}>
-                    <input/>
+                        <select className={'searchList'}>
+                            <option>All</option>
+                            <option>Deals</option>
+                            <option>Alexa Skills</option>
+                            <option>Amazon Devices</option>
+                            <option>Amazon Fashion</option>
+                            <option>Amazon Pantry</option>
+                            <option>Appliances</option>
+                            <option>Apps and Games</option>
+                            <option>Groceries and Gourmet Foods</option>
+                            <option>Tools and Home Improvement</option>
+                        </select>
+                    <input placeholder={'Search'} className={'searchInput'}/>
+                    <div className={'searchBtn'}><SearchIcon className={'searchIcon'}/></div>
                 </div>
+
                 <div className={'tabs'}>
-                    <div className={'tabOne'}>Hello SignIn</div>
-                    <div className={'tabTwo'}>Return and Orders</div>
-                    <div className={'tabThree'}>Cart</div>
+                    <div className={'tabOne'}>Hello, Sign in <span>Account and Lists</span></div>
+                    <div className={'tabTwo'}>Returns<span>& Orders</span></div>
+                    <div className={'tabThree'}>
+                        <ShoppingCartIcon className={'cartIcon'}/>
+                        <span>Cart</span>
+                    </div>
                 </div>
+
             </div>
+
             <div className={'navbarTwo'}>
 
             </div>
+
         </div>
     )
 
